1a416c93b5e2d9a4824c903b780504709009540f
Niklas Neumann <niklasneumann0@gmail.com>
2026-05-25 12:58:06 +0200

    Add destination NAT module utils

---
+++ plugins/module_utils/main/nat_destination.py
@@ -0,0 +1,98 @@
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.api import \
+    Session
+from ansible_collections.oxlorg.opnsense.plugins.module_utils.helper.validate import \
+    is_unset
+from ansible_collections.oxlorg.opnsense.plugins.module_utils.helper.rule import \
+    validate_values
+from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.cls import BaseModule
+
+
+class DNat(BaseModule):
+    CMDS = {
+        'add': 'add_rule',
+        'del': 'del_rule',
+        'detail': 'get_rule',
+        'set': 'set_rule',
+        'search': 'search_rule',
+        'toggle': 'toggle_rule',
+    }
+    API_KEY_PATH = 'nat.rule'
+    API_MOD = 'firewall'
+    API_CONT = 'd_nat'
+    FIELDS_CHANGE = [
+        'sequence', 'interface', 'target', 'target_port', 'description',
+        'ip_protocol', 'protocol', 'source_invert', 'source_net', 'source_port',
+        'destination_invert', 'destination_net', 'destination_port', 'log',
+    ]
+    FIELDS_ALL = ['enabled']
+    FIELDS_ALL.extend(FIELDS_CHANGE)
+    FIELDS_BOOL_INVERT = ['enabled']
+    FIELDS_TRANSLATE = {
+        'enabled': 'disabled',
+        'ip_protocol': 'ipprotocol',
+        'source_invert': ('source', 'not'),
+        'source_net': ('source', 'network'),
+        'source_port': ('source', 'port'),
+        'destination_invert': ('destination', 'not'),
+        'destination_net': ('destination', 'network'),
+        'destination_port': ('destination', 'port'),
+        'target_port': 'local-port',
+        'description': 'descr',
+    }
+    FIELDS_TYPING = {
+        'bool': ['enabled', 'log', 'source_invert', 'destination_invert'],
+        'list': [],
+        'select': ['interface', 'ip_protocol', 'protocol'],
+        'int': [],
+    }
+    INT_VALIDATIONS = {
+        'sequence': {'min': 1, 'max': 999999},
+    }
+    EXIST_ATTR = 'rule'
+    API_CMD_REL = 'apply'
+
+    def __init__(self, module: AnsibleModule, result: dict, session: Session = None, fail: dict = None):
+        BaseModule.__init__(self=self, m=module, r=result, s=session, f=fail)
+        self.rule = {}
+
+    def check(self) -> None:
+        if self.p['state'] == 'present':
+            if is_unset(self.p['interface']):
+                self.m.fail_json(
+                    "You need to provide an 'interface' to create a destination-nat rule!"
+                )
+
+            if is_unset(self.p['target']):
+                self.m.fail_json(
+                    "You need to provide an 'target' to create a destination-nat rule!"
+                )
+
+        self._build_log_name()
+        self.b.find(match_fields=self.p['match_fields'])
+
+        if self.p['state'] == 'present':
+            validate_values(module=self.m, cnf=self.p, error_func=self.m.fail_json, kind='nat')
+
+        self._base_check()
+
+    def _build_log_name(self) -> str:
+        if self.p['description'] not in [None, '']:
+            log_name = self.p['description']
+
+        else:
+            log_name = 'FROM '
+
+            if self.p['source_invert']:
+                log_name += 'NOT '
+
+            log_name += f"{self.p['source_net']} <= PROTO {self.p['protocol']} => "
+
+            if self.p['destination_invert']:
+                log_name += 'NOT '
+
+            log_name += f"{self.p['destination_net']}:{self.p['destination_port']} "
+            log_name += f"=DNAT=> {self.p['target']}:{self.p['target_port']}"
+
+        return log_name
